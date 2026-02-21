bc6d83ae8d96dce5fee92c871703843334edfed4
Krish Lahre <130497736+your-krish@users.noreply.github.com>
2026-02-21 22:15:43 +0530

    Update supabase client initialization method

--- js/config.js
+++ js/config.js
@@ -8,4 +8,5 @@ const MAX_POSTS_PER_DAY = 4;
 const MAX_IMAGE_SIZE_BYTES = 3 * 1024 * 1024;
 
 const { createClient } = supabase;
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window._supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window._supabase;
